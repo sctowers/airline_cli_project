ad44f81869c633b529a53dc605e824608f0908e8
Piraven Nandakumaran <Xothspath@gmail.com>
2023-10-24 11:34:09 +0100

    Completed passenger which has name, contact infomation and an ID and passed all tests

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,42 @@
 public class Passenger {
+
+    private String passengerID;
+    private String name;
+    private String contactInfo;
+
+    public Passenger (String passengerID, String name, String contactInfo){
+        this.contactInfo = contactInfo;
+        this.name = name;
+        this.passengerID = passengerID;
+    }
+
+    public String getPassengerInfo(){
+        return "Name: "+ this.name +
+                    " PassengerID: "+this.passengerID+
+                    " Passenger Contact Info: "+this.contactInfo;
+    }
+
+    public String getPassengerID() {
+        return passengerID;
+    }
+
+    public void setPassengerID(String passengerID) {
+        this.passengerID = passengerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,4 +1,53 @@
-public class PassengerTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-    passenger
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PassengerTest {
+    private Passenger passenger;
+
+    @BeforeEach
+
+    public void setUp(){
+
+        passenger = new Passenger("HUY789","Piraven","BobF@hotmail.com");
+    }
+
+    @Test
+    public void canGetPassengerID(){
+
+        assertThat(passenger.getPassengerID()).isEqualTo("HUY789");
+    }
+    @Test
+    public void canGetPassengerName(){
+
+        assertThat(passenger.getName()).isEqualTo("Piraven");
+    }
+    @Test
+    public void canGetPassengerContactInfo(){
+
+        assertThat(passenger.getContactInfo()).isEqualTo("BobF@hotmail.com");
+    }
+    @Test
+    public void canSetPassengerID(){
+        passenger.setPassengerID("hhh");
+        assertThat(passenger.getPassengerID()).isEqualTo("hhh");
+    }
+    @Test
+    public void canSetPassengerName(){
+        passenger.setName("Suzi");
+        assertThat(passenger.getName()).isEqualTo("Suzi");
+    }
+    @Test
+    public void canSetPassengerContactInfo(){
+        passenger.setContactInfo("Sunny@gmail.com");
+        assertThat(passenger.getContactInfo()).isEqualTo("Sunny@gmail.com");
+    }
+
+    @Test
+    public void canGetPassengerInfo(){
+        String expectedInfo = "Name: Piraven PassengerID: HUY789 Passenger Contact Info: BobF@hotmail.com";
+        assertThat(passenger.getPassengerInfo()).isEqualTo(expectedInfo);
+    }
+
 }
